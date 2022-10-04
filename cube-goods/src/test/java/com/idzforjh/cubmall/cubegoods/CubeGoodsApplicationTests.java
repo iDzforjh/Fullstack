13312035fdde6be6e584f49ff618505a5a54ac8d
iDzforjh <1486702069@qq.com>
2022-10-04 16:42:44 +0800

    environment create
    first commit.

---
+++ cube-goods/src/test/java/com/idzforjh/cubmall/cubegoods/CubeGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idzforjh.cubmall.cubegoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubeGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
